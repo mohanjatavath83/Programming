4c035399bad602299b7f0bca52cd256acb8e151c
mohan.jatavath <mohan.jatavath@ril.com>
2021-10-11 08:34:36 +0530

    arrys

--- src/com/mohan/ArraysBasics.java
+++ src/com/mohan/ArraysBasics.java
@@ -1,5 +1,7 @@
 package com.mohan;
 
+import java.util.Random;
+
 public class ArraysBasics {
 
 
@@ -7,5 +9,17 @@ public class ArraysBasics {
 
 
         int[] arr = new int[10];
+
+        Random r = new Random();
+        for(int i =0;i<arr.length;i++)
+        {
+            arr[i]=r.nextInt(10);
+        }
+
+        for(int i =0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
     }
 }
