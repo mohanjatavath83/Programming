26f93f720903a3e480a0295e1452421e4c40ac7c
mohan.jatavath <mohan.jatavath@ril.com>
2021-10-21 20:57:19 +0530

    second

--- src/com/mohan/array/MaxElementInArray.java
+++ src/com/mohan/array/MaxElementInArray.java
@@ -18,32 +18,17 @@ public class MaxElementInArray {
             System.out.print(arr[i] + " ");
 
 
-        int max1;
-        int max2;
+        int max1 = arr[0];
 
-        if(arr[0]>arr[1]) {
-            max1=arr[0];
-            max2 =arr[1];
-        }else
+
+        for (int i = 1; i < arr.length; i++)
         {
-            max1 = arr[1];
-            max2=arr[0];
-        }
 
-
-        for (int i = 2; i < arr.length; i++) {
-
-            if (max1 < arr[i]) {
-                max2 = max1;
+            if (max1 < arr[i])
                 max1 = arr[i];
-            }else if(arr[i]>max2 && arr[i]!=max1)
-            {
-                max2 = arr[i];
-            }
         }
 
         System.out.println("\nmaximum element in this array = " + max1);
-        System.out.println("\nsecond maximum element in this array = " + max2);
 
     }
 }
