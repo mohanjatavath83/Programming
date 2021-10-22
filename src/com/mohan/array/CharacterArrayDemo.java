cfc99067e4bdd5e7cf075df62dd33c43bc980530
mohan.jatavath <mohan.jatavath@ril.com>
2021-10-22 20:41:18 +0530

    selection sort

---
+++ src/com/mohan/array/CharacterArrayDemo.java
@@ -0,0 +1,32 @@
+package com.mohan.array;
+
+import java.sql.SQLOutput;
+
+public class CharacterArrayDemo {
+
+
+    public static void main(String[] args) {
+
+
+        char ch = 'A';
+        System.out.println(ch);
+
+
+        char[] name = {'M', 'O', 'H', 'A', 'N'};
+        System.out.println(name);
+
+
+        int[] arr = {1,2,3,4};
+        System.out.println(arr);
+
+
+        System.out.println(arr.length);
+
+
+
+
+
+    }
+
+
+}
---
+++ src/com/mohan/array/SortingArray.java
@@ -0,0 +1,55 @@
+package com.mohan.array;
+
+import java.util.Random;
+
+public class SortingArray {
+
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[200];
+
+        Random random = new Random();
+
+        for(int i =0; i<arr.length;i++)
+        {
+            arr[i]=random.nextInt(100);
+        }
+
+
+
+        for(int i =0; i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+
+        //selection sort
+
+        for(int j =0;j<arr.length;j++)
+        {
+            int max = arr[0];
+            int index = 0;
+
+            for (int i = 0; i <= arr.length-j-1; i++) {
+                if (max < arr[i]) {
+                    max = arr[i];
+                    index = i;
+                }
+            }
+
+            int temp = arr[index];
+            arr[index]= arr[arr.length-j-1];
+            arr[arr.length-j-1] = temp;
+        }
+
+        System.out.println();
+
+        for(int i =0; i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+
+    }
+}
